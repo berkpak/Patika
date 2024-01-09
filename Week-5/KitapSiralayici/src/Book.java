446b2d821cb5f3633c4dd7ae700aac5f1f5abda3
berkpak <pakkberk@gmail.com>
2024-01-09 21:35:04 +0300

    edited

--- Week-5/KitapSiralayici/src/Book.java
+++ Week-5/KitapSiralayici/src/Book.java
@@ -1,26 +1,52 @@
 import java.util.Comparator;
 
-public class Book implements Comparable {
+public class Book implements Comparable<Book> {
     private String name;
     private int page;
+    private String author;
+    private String releaseDate;
 
     //Comparable interfaceden gelen compareTo metodunu override yap
     @Override
-    public int compareTo(Object o) {
-        return 0;
+    public int compareTo(Book o) {
+        return this.getName().compareTo(o.getName());
     }
 
-    public Book(String name, int page) {
+    public Book(String name, int page, String author, String releaseDate) {
         this.name = name;
         this.page = page;
+        this.author = author;
+        this.releaseDate = releaseDate;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
     public int getPage() {
         return page;
     }
 
+    public void setPage(int page) {
+        this.page = page;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
 }
--- Week-5/KitapSiralayici/src/Main.java
+++ Week-5/KitapSiralayici/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
     public static void main(String[] args) {
 
         //Book sinifindan yeni kitap nesnesi uret
-        Book newBook1 = new Book("Yuzuklerin Efendisi",1000);
-        Book newBook2 = new Book("Sherlock Holmes",400);
-        Book newBook3 = new Book("Dune",600);
-        Book newBook4 = new Book("Narnia Gunlukleri",900);
-        Book newBook5 = new Book("Harry Potter",500);
+        Book newBook1 = new Book("Yuzuklerin Efendisi",1000,"J.R.R Tolkien","1954");
+        Book newBook2 = new Book("Sherlock Holmes",400,"Arthur Conan Doyle","1887");
+        Book newBook3 = new Book("Dune",600,"Frank Herbert","1965");
+        Book newBook4 = new Book("Narnia Gunlukleri",900,"Clive Staples Lewis","1950");
+        Book newBook5 = new Book("Harry Potter",500,"J. K. Rowling","1997");
 
         //bookPage adinda set yapisinda sakla
         TreeSet<Book> bookPage = new TreeSet<>(new ComparatorByPage());
@@ -25,11 +25,11 @@ public class Main {
         System.out.println();
         //Kitaplari saya sayisina gore sirala
         for(Book book : bookPage){
-            System.out.println(book.getName() + " --> " + book.getPage());
+            System.out.println(book.getName() + " - " + book.getPage() + " - " + book.getAuthor() +" "+ book.getReleaseDate());
         }
 
         //bookName adinda set yapisinda sakla
-        TreeSet<Book> bookName = new TreeSet<>(new ComparatorByName());
+        TreeSet<Book> bookName = new TreeSet<>();
 
         //bookName.add(new Book("Yuzuklerin Efendisi",100));
         bookName.add(newBook1);
@@ -43,7 +43,7 @@ public class Main {
         System.out.println();
         //Kitaplari isme gore sirala
         for (Book book2 : bookName){
-            System.out.println(book2.getName());
+            System.out.println(book2.getName()+" - " + book2.getPage() + " - " + book2.getAuthor() +" "+ book2.getReleaseDate());
         }
     }
 }
